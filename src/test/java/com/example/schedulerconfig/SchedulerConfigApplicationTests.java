5c7592123358db021c9c459c506efb6f73113170
RobbyQuintiensPXL <robby.quintiens@student.pxl.be>
2023-10-30 18:32:18 +0100

    properties

---
+++ src/test/java/com/example/schedulerconfig/SchedulerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulerconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
